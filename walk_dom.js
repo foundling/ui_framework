a4091fa7e5f79deea9807112b60d360c7cf501cb
Alex Ramsdell <alexramsdell@gmail.com>
2016-10-25 11:54:35 -0700

    index.html

---
+++ walk_dom.js
@@ -0,0 +1,25 @@
+let results = [];
+
+const walk_dom = function(node, func) {
+
+  func(node);
+  node = node.firstChild;
+  while (node) {
+      walk_dom(node, func);
+      node = node.nextSibling;
+  }
+
+};
+
+const find_framework_nodes = function() {
+
+  if (node.nodeType === 1) {
+      if (node.hasAttribute('xyz')) {
+        results.push(node);
+      }
+  }
+
+};
+
+walk_dom(document.body, find_framework_nodes);
+console.log(results);
